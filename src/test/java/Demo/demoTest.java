4232b35a9262c45ea55981cd1f969c13535b0de8
nagarajgit48 <nagaraj481989@gmail.com>
2026-08-28 10:28:35 +0530

    commited

---
+++ src/test/java/Demo/demoTest.java
@@ -0,0 +1,15 @@
+package Demo;
+
+import org.testng.annotations.Test;
+
+public class demoTest {
+
+	
+	@Test(groups="smoke")
+		public void addTest()
+		{
+		System.out.println("add");
+		}
+	
+	
+}
